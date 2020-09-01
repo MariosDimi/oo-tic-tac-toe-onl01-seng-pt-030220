d8959bcccbb66609b97b54732c25636fcc5b482c
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2020-09-01 21:39:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,3 +27,36 @@ class TicTacToe
     user_input.to_i - 1
   end
   
+  def move(index, current_player = "X")
+    @board[index] = current_player
+  end
+  
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == "")
+  end
+  
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+  
+  def turn
+    puts "Please choose a number 1-9:"
+    user_input = gets.chomp
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      player_token = current_player
+      move(index, player_token)
+      display_board
+    else
+      turn
+    end
+  end
+  
+  def turn_count
+    @board.count{|token| token == "X" ||token == "O"}
+  end
+  
+  def current_player
+    turn_count % 2 == O? "X" :"O"
+  end
+  
